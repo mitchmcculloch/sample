45196f10f8458c8cef30fbae211365278c52d2a7
Mitch Mcculloch <mitchell.mcculloch28+gh@gmail.com>
2019-01-20 14:53:23 -0500

    Added some placeholders to existing pages

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Listing from '../components/listing'
+
+//Completely placeholder
+const Blog = ({ location }) => (
+  <Layout location={location}>
+    <Listing />
+  </Layout>
+)
+
+export default Blog
---
+++ src/pages/menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Listing from '../components/listing'
+
+//Completely placeholder
+const Menu = ({ location }) => (
+  <Layout location={location}>
+    <Listing />
+  </Layout>
+)
+
+export default Menu
